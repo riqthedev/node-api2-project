b986da8505d77f4f421b8a88b53c326d13e5fca6
riqthedev <riqthedev@gmail.com>
2021-09-29 08:35:37 -0400

    solved taks 1-4

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // require your server and launch it here
+const { json } = require("express");
+
+const server = require('./api/server');
+
+const port = 5000;
+
+// START YOUR SERVER HERE
+
+server.listen(port, () => {
+    console.log("listening on", port )
+})
